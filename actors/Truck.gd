5446939b1da6a19abced576b6ba9be8b420a24a0
Bloodyaugust <greysonrichey@gmail.com>
2023-05-01 14:40:01 -0600

    Add music
    - Add sfx for truck
    - Change truck to be "started up" during run status view

--- actors/Truck.gd
+++ actors/Truck.gd
@@ -1,6 +1,8 @@
 extends AnimatedSprite2D
 
 @onready var _truck_exhaust: CPUParticles2D = %TruckExhaust
+@onready var _truck_idle_player: AudioStreamPlayer2D = %TruckIdlePlayer
+@onready var _truck_startup_player: AudioStreamPlayer2D = %TruckStartupPlayer
 
 var _tween: Tween
 
@@ -12,24 +14,31 @@ func _on_store_state_changed(state_key: String, substate) -> void:
           if _tween:
             _tween.kill()
 
+          _truck_startup_player.play()
           _truck_exhaust.emitting = true
           play()
           _tween = create_tween().set_trans(Tween.TRANS_LINEAR)
+          _tween.tween_interval(1.7)
+          _tween.tween_callback(func(): _truck_idle_player.play())
           _tween.tween_property(self, "global_position", Vector2(0.0, global_position.y), 1.0)
           _tween.tween_callback(func():
             Store.set_state("game", GameConstants.GAME_RESULTS)
             ViewController.set_client_view(ViewController.CLIENT_VIEWS.RESULTS)
             pause()
-            _truck_exhaust.emitting = false
           )
         GameConstants.GAME_OVER:
           if _tween:
             _tween.kill()
 
           _tween = create_tween().set_trans(Tween.TRANS_LINEAR)
+
           if abs(global_position.x) > 0.1:
+            _tween.tween_interval(1.7)
+            _tween.tween_callback(func(): _truck_idle_player.play())
             _tween.tween_property(self, "global_position", Vector2(0.0, global_position.y), 1.0)
             _tween.tween_property(self, "global_position", Vector2(0.0, global_position.y), 2.0)
+            _truck_startup_player.play()
+
           _tween.tween_callback(func(): ViewController.set_client_view(ViewController.CLIENT_VIEWS.MAIN_MENU))
           play()
           _truck_exhaust.emitting = true
@@ -37,6 +46,7 @@ func _on_store_state_changed(state_key: String, substate) -> void:
         GameConstants.GAME_STARTING:
           pause()
           _truck_exhaust.emitting = false
+          _truck_idle_player.stop()
 
 func _process(delta):
   match Store.state.game:
